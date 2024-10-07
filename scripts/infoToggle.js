b3e56033ba7e9c5aae955723fa145ee4d34035d7
Ryan Gleeson <71162701+PracticalRyan@users.noreply.github.com>
2024-10-07 14:24:51 +0700

    reformatted code

--- scripts/infoToggle.js
+++ scripts/infoToggle.js
@@ -1,14 +1,13 @@
-const modal_container = document.getElementById("infoContainer")
-let shown = false
+const modal_container = document.getElementById("infoContainer");
+let shown = false;
 document.getElementById("infoButton").onclick = function () {
-    if (shown == false) {
-        modal_container.classList.add('show')
-        shown = true
-        console.log("Container shown");
-    }
-    else {
-        modal_container.classList.remove('show')
-        shown = false
-        console.log("Container hidden");
-    }
-}
+  if (shown == false) {
+    modal_container.classList.add("show");
+    shown = true;
+    console.log("Container shown");
+  } else {
+    modal_container.classList.remove("show");
+    shown = false;
+    console.log("Container hidden");
+  }
+};
